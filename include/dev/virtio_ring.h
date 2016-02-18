10d97c367dc589bca24ebf6c3d115967d9cabced
haiyang1992 <haiyang1992@gmail.com>
2016-02-18 16:47:58 -0600

    virtio basics

--- include/dev/virtio_ring.h
+++ include/dev/virtio_ring.h
@@ -72,13 +72,6 @@ struct virtq_desc {
         le64 addr;
         /* Length. */
         le32 len;
-        
-/* This marks a buffer as continuing via the next field. */
-#define VRING_DESC_F_NEXT 1
-/* This marks a buffer as write-only (otherwise read-only). */
-#define VRING_DESC_F_WRITE 2
-/* This means the buffer contains a list of buffer desctriptors. */
-#define VRINGDESC_F_INDIRECT 4
 	/* The flags as indicated above. */
         le16 flags;
         /* We chain unused descriptors via this, too */
